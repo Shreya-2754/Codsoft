5fb4b224b9103a1b063302ea6adcc167be23b02a
Shreya-2754 <shreyamallick27@gmail.com>
2025-06-10 15:41:01 +0530

    Add files via upload

---
+++ Task_2.cpp
@@ -0,0 +1,30 @@
+//Task_2
+// Simple Calculator
+#include <iostream>
+using namespace std;
+int main() {
+    double num1, num2;
+    char op;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+    if (op == '+') {
+        cout << "Result: " << num1 + num2 << endl;
+    } else if (op == '-') {
+        cout << "Result: " << num1 - num2 << endl;
+    } else if (op == '*') {
+        cout << "Result: " << num1 * num2 << endl;
+    } else if (op == '/') {
+        if (num2 != 0) {
+            cout << "Result: " << num1 / num2 << endl;
+        } else {
+            cout << "Error: Cannot divide by zero!" << endl;
+        }
+    } else {
+        cout << "Invalid operator!" << endl;
+    }
+    return 0;
+}
